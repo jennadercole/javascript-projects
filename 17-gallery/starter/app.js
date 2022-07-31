7b26e6a69118d98e467dba7dba82d4ce648d5554
Jenna <jennardercole@gmail.com>
2022-07-31 11:33:50 -0500

    Halfway through gallery project

--- 17-gallery/starter/app.js
+++ 17-gallery/starter/app.js
@@ -7,3 +7,24 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element) {
+  this.container = element;
+  this.list = [...element.querySelectorAll(".img")];
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.modalImages = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.nextBtn = getElement(".next-btn");
+  this.prevBtn = getElement(".prev-btn");
+  //bind functions
+  this.openModal = this.openModal.bind(this);
+  this.container.addEventListener("click", this.openModal);
+}
+
+Gallery.prototype.openModal = function () {
+  this.modal.classList.add("open");
+};
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
