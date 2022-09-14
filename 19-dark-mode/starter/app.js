26b71ef6d232b8c2091fbf330fb7c34e0e406645
Jenna <jennardercole@gmail.com>
2022-09-13 19:58:42 -0500

    Dark mode project complete, attempted to use Moment.js but integrity attribute throws an error

--- 19-dark-mode/starter/app.js
+++ 19-dark-mode/starter/app.js
@@ -1,1 +1,23 @@
-console.log('dark mode');
+const toggle = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+toggle.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+
+    return `<article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${date}</span>
+            <span>${length} min read</span>
+          </div>
+          <p>${snippet}</p>
+        </article>`;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articlesData;
